010603906a28fe5446753a5cf92f8b2138c8a9a1
Shane Swezey <shanedswezey@gmail.com>
2018-11-09 17:16:33 -0800

    Updated Married jointly path

--- routes/marriedj.js
+++ routes/marriedj.js
@@ -5,5 +5,6 @@ const MarriedJController = require('../controllers/marriedjcontroller');
 
 router.get("/", MarriedJController.marriedj_get_all);
 router.get("/:year", MarriedJController.marriedj_get_year);
+router.get("/:year/:income", MarriedJController.marriedj_get_taxBreakdown);
 
 module.exports = router;
