35e5bf97b19a9365e3169e5ef8ab0993f3f7b122
Shane Swezey <shanedswezey@gmail.com>
2018-11-10 20:03:26 -0800

    Added taxBreakdown path

--- routes/household.js
+++ routes/household.js
@@ -5,5 +5,6 @@ const HouseHoldController = require('../controllers/householdcontroller');
 
 router.get("/", HouseHoldController.household_get_all);
 router.get("/:year", HouseHoldController.household_get_year);
+router.get("/:year/:income", HouseHoldController.household_get_taxBreakdown);
 
 module.exports = router;
