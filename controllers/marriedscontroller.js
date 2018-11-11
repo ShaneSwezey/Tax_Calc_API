dfcd4078715e917d4a6a7c14ee958f0957582476
Shane Swezey <shanedswezey@gmail.com>
2018-11-10 19:54:51 -0800

    Added marrieds_get_taxBreakdown function

--- controllers/marriedscontroller.js
+++ controllers/marriedscontroller.js
@@ -12,7 +12,7 @@ exports.marrieds_get_all = (req, res, next) => {
         if (docs) {
             res.status(200).json(docs);
         } else {
-            res.status(404).json({ message: "No valid entry found for provided year"})
+            res.status(404).json({ message: "No valid entry found for provided year"});
         }
     })
     .catch(err => {
@@ -32,13 +32,7 @@ exports.marrieds_get_year = (req, res, next) => {
     .then(fileYear => {
         console.log("From database", fileYear);
         if (fileYear) {
-            res.status(200).json({
-                fileYear: fileYear,
-                request: {
-                    type: "GET",
-                    url: "http://localhost:3001/marrieds"
-                }
-            });
+            res.status(200).json(fileYear);
         } else {
             res.status(404).json({ message: "No valid entry found for provided year"});
         }
@@ -49,4 +43,40 @@ exports.marrieds_get_year = (req, res, next) => {
             errror: err
         })
     });
+};
+
+// Http: Get
+// Returns json object containing tax data for married joint filing by year
+exports.marrieds_get_taxBreakdown = (req, res, next) => {
+    MarriedSFiler.findOne( { year: req.params.year } )
+    .select("year rates _id")
+    .exec()
+    .then(fileYear => {
+        console.log("From database", fileYear);
+        if (fileYear) {
+            let taxBracket = TaxCalculator.calculateBracket(fileYear.rates, req.params.income);
+            let taxAmount = TaxCalculator.calculateTax(fileYear.rates, req.params.income);
+            let socialSecurityTax = PayRollCalculator.calculateSocialSecurityTax(req.params.income);
+            let medicareTax = PayRollCalculator.calculateMedicareTax(req.params.income, 'marrieds');
+            let percentOfIncome = TaxCalculator.calculateTaxAsPercentageOfIncome(req.params.income, taxAmount + socialSecurityTax + medicareTax);
+            const taxInfo = {
+                year: fileYear.year,
+                taxBracket: taxBracket,
+                taxAmount: taxAmount,
+                percentOfIncome: percentOfIncome,
+                socialSecurityTax: socialSecurityTax,
+                medicareTax: medicareTax,
+                rates: fileYear.rates
+            };
+            res.status(200).json(taxInfo);
+        } else {
+            res.status(404).json({ message: "No valid entry found for provided year" });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    })
 };
