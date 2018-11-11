5990d3fcdab9266c6ada9e9c9240299738cd295a
Shane Swezey <shanedswezey@gmail.com>
2018-11-10 19:57:12 -0800

    Added year/income path

--- routes/marrieds.js
+++ routes/marrieds.js
@@ -5,5 +5,6 @@ const MarriedSController = require('../controllers/marriedscontroller');
 
 router.get("/", MarriedSController.marrieds_get_all);
 router.get("/:year", MarriedSController.marrieds_get_year);
+router.get("/:year/:income", MarriedSController.marrieds_get_taxBreakdown);
 
 module.exports = router;
